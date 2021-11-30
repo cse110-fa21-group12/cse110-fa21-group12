3ce90d7d017bd73a65bab4d72192a317b0c26c1f
immmjack <yanggangjack@gmail.com>
2021-11-29 20:11:03 -0800

    start testing

---
+++ testing/e2e_test.js
@@ -0,0 +1,8 @@
+describe('Google', () => {
+    beforeAll(() => {
+        await page.goto('http://google.com');
+    });
+    it('It should display Google', async() => {
+        await expect(page.title()).resolves.toMatch('Google');
+    });
+});
