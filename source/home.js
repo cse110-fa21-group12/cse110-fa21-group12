5df9a1747ce978555c0be6b8854022ebdc718f81
nataliekwong925 <nataliekwong925@users.noreply.github.com>
2021-12-07 09:30:21 +0000

    Prettified Code!

--- source/home.js
+++ source/home.js
@@ -1,32 +1,30 @@
 //Search functionality
-function search(){
-    const searchWord = document.getElementsByClassName("search")[0].value;
-    console.log(searchWord);
+function search() {
+  const searchWord = document.getElementsByClassName("search")[0].value;
+  console.log(searchWord);
 
-    fetch("/search/" + searchWord, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          console.log("Success:", data);
-        });
-
-
+  fetch("/search/" + searchWord, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Success:", data);
+    });
 }
 
 //Search if the search button is clicked or if user hits enter
 const searchButton = document.getElementsByClassName("fa fa-search")[0];
 const searchText = document.getElementsByClassName("search")[0];
 
-searchText.addEventListener("keypress", function(e){
-    if(e.key=="Enter"){
-        search();
-    }
-})
+searchText.addEventListener("keypress", function (e) {
+  if (e.key == "Enter") {
+    search();
+  }
+});
 
-searchButton.addEventListener("click", function() {
-    search;
-})
+searchButton.addEventListener("click", function () {
+  search;
+});
