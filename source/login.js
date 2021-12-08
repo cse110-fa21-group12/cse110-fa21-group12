b69b9061c8bd91dccc94ea3e95ef60d06bfc4712
lbove27 <lbove27@users.noreply.github.com>
2021-12-08 05:35:52 +0000

    Prettified Code!

--- source/login.js
+++ source/login.js
@@ -1,27 +1,26 @@
-const email = document.getElementById('email-input').value;
-const password = document.getElementById('password-input').value;
-const loginButton = document.getElementsByClassName('login-button')[0];
+const email = document.getElementById("email-input").value;
+const password = document.getElementById("password-input").value;
+const loginButton = document.getElementsByClassName("login-button")[0];
 
 const jsonUserData = {
-    email: email,
-    password: password,
+  email: email,
+  password: password,
 };
 
-loginButton.addEventListener('click', function() {
-    fetch("/user/sign-in", {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(jsonUserData),
-    })
+loginButton.addEventListener("click", function () {
+  fetch("/user/sign-in", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(jsonUserData),
+  })
     .then((response) => response.json())
     .then((data) => {
       console.log("Success:", data);
-      if(data.error == "Invalid email") {
+      if (data.error == "Invalid email") {
         location.reload();
-      }
-      else {
+      } else {
         location.href = "home.html";
       }
     })
