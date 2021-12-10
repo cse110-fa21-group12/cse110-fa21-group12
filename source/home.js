5b7e86358cc18dd81be1aaeefa2f1e460ef06f06
nataliekwong925 <nataliekwong925@users.noreply.github.com>
2021-12-10 03:28:24 +0000

    Prettified Code!

--- source/home.js
+++ source/home.js
@@ -25,39 +25,38 @@ function displayRecipes(data) {
   }
 
   for (let i = 0; i < data.length; i++) {
-      //Get list of recipes to append each recipe card to
-      const list = document.getElementsByClassName("recipe-list")[0];
+    //Get list of recipes to append each recipe card to
+    const list = document.getElementsByClassName("recipe-list")[0];
 
-      //Create recipe card div to append elements to
-      const initialDiv = document.createElement("div");
-      initialDiv.setAttribute("class", "recipe");
-      initialDiv.setAttribute("id", data[i].title);
-      initialDiv.href = "recipe.html";
-      initialDiv.addEventListener("click", function (event) {
-        localStorage.setItem("id", data[i].title);
-        window.location = "recipe.html";
-      });
+    //Create recipe card div to append elements to
+    const initialDiv = document.createElement("div");
+    initialDiv.setAttribute("class", "recipe");
+    initialDiv.setAttribute("id", data[i].title);
+    initialDiv.href = "recipe.html";
+    initialDiv.addEventListener("click", function (event) {
+      localStorage.setItem("id", data[i].title);
+      window.location = "recipe.html";
+    });
 
-  
-      //Create and add image to the recipe card
-      const recipeImage = document.createElement("img");
-      recipeImage.setAttribute("src", data[i].img);
-      initialDiv.appendChild(recipeImage);
+    //Create and add image to the recipe card
+    const recipeImage = document.createElement("img");
+    recipeImage.setAttribute("src", data[i].img);
+    initialDiv.appendChild(recipeImage);
 
-      //Create and add recipe name to the recipe card
-      const recipeName = document.createElement("p");
-      recipeName.setAttribute("class", "recipes-title");
-      recipeName.innerHTML = data[i].title;
-      initialDiv.appendChild(recipeName);
+    //Create and add recipe name to the recipe card
+    const recipeName = document.createElement("p");
+    recipeName.setAttribute("class", "recipes-title");
+    recipeName.innerHTML = data[i].title;
+    initialDiv.appendChild(recipeName);
 
-      //Create and add cooking time to the recipe card
-      const timeTaken = document.createElement("p");
-      timeTaken.setAttribute("class", "recipe-time");
-      timeTaken.innerHTML =
-        '<i class="fas fa-clock">  ' + data[i].totalTime + " minutes";
-      initialDiv.appendChild(timeTaken);
+    //Create and add cooking time to the recipe card
+    const timeTaken = document.createElement("p");
+    timeTaken.setAttribute("class", "recipe-time");
+    timeTaken.innerHTML =
+      '<i class="fas fa-clock">  ' + data[i].totalTime + " minutes";
+    initialDiv.appendChild(timeTaken);
 
-      list.appendChild(initialDiv);
+    list.appendChild(initialDiv);
   }
 }
 
